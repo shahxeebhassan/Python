e69864c31172aa26c848f699dec891d8053de5c4
Shahzaib Hassan <shahxeebhassan@gmail.com.com>
2023-02-11 16:35:20 +0500

    Conitionals

---
+++ Topic_Wise/2Conditionals/Even.py
@@ -0,0 +1,12 @@
+def main():
+    x = int(input("Enter a Number : "))
+    print(isEven(x))
+def isEven(x):
+    # if x % 2 == 0:
+    #     return True
+    # else:
+    #     return False
+    return True if x % 2 == 0 else False
+
+
+main()
---
+++ Topic_Wise/2Conditionals/house.py
@@ -0,0 +1,29 @@
+def main():
+    x = input("Enter your Name: ")
+    house(x)
+    # names(x)
+    match x:
+        case "Shahzaib":
+            print("Loves Afnan")
+        case "Afnan" | "Zara":
+            print("Loves Shahzaib")
+        case _:
+            print("Sorry i've no information about that")
+def house(z):
+    # z.strip();
+    if z == "Shahzaib" or z == "Rabia" or z == "Sehrish":
+        print("Bahawalpur")
+    elif z == "Imran" or z == "Usman":
+        print("Ahmadpur")
+    else:
+        print("Who?")
+
+def names(z):
+    # match z :
+    #     case "Shahzaib":
+    #         print("Loves Afnan")
+    #     case "Afnan" | "Zara":
+    #         print("Loves Shahzaib")
+    #     case _:
+    #         print("Sorry i've no information about that")
+main()
